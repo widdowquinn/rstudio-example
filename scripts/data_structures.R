d357152162a0d5a9a0899c64e26ede526b686c19
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2018-01-12 15:25:42 +0000

    add scripts from second session

--- scripts/data_structures.R
+++ scripts/data_structures.R
@@ -23,3 +23,35 @@ sd_by_day <- apply(data, 2, sd)
 plot(sd_by_day)
 
 hist(avg_inflammation_day, breaks=seq(0, 15, by=0.5))
+
+
+# Afternoon session - data structures
+
+# integer vector
+x <- c(10, 12, 45, 33)
+
+# Define second vector
+xx <- c(1, 2, 'a')
+
+# Define factor
+f <- factor(c("no", "yes", "no"))
+
+# Create a matrix of zeroes
+m1 <- matrix(0, ncol=6, nrow=3)
+
+# Create a list
+l <- list(1, "a", TRUE, matrix(0, nrow=2, ncol=2), f)
+
+# Create named list
+l_named <- list(course="SWC", b=1:4)
+
+# Create vector
+v <- c(5.4, 6.2, 7.1, 4.8, 7.5)
+mask <- c(TRUE, FALSE, TRUE, FALSE, TRUE)
+
+# Create dataframe
+df <- data.frame(a=1:3, b=c("eeny", "meeny", "miney"), c=c(TRUE, FALSE, TRUE))
+
+
+
+
---
+++ scripts/gapminder.R
@@ -0,0 +1,2 @@
+# Load gapminder data from URL
+gapminder <- read.table("https://raw.githubusercontent.com/resbaz/r-novice-gapminder-files/master/data/gapminder-FiveYearData.csv" , sep=",", header=TRUE)
